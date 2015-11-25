8c35b42501a125a9061f99c9a2e48fa085f56643
miskens@hotmail.com <miskens@hotmail.com>
2015-11-25 14:25:14 +0100

    Added groups model

---
+++ LexiconLMS/Models/Group.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Benämning")]
+        public string Name { get; set; }
+
+        [Display(Name = "Beskrivning")]
+        public string Description{ get; set; } 
+
+        [Display(Name = "Startdatum")]
+        public DateTime StartDate{ get; set; }
+
+        [Display(Name = "Slutdatum")]
+        public DateTime EndDate { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+    }
+}
