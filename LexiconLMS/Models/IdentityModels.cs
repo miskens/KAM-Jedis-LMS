4cb9fd6b93d76b835b5c33597a9a8a03f021c0b0
kenfo1 <kenneth.forsstrom@hotmail.com>
2015-11-25 11:11:59 +0100

    Usersmodel new properties and seed

--- LexiconLMS/Models/IdentityModels.cs
+++ LexiconLMS/Models/IdentityModels.cs
@@ -3,12 +3,21 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace LexiconLMS.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name= "Namn")]
+        public string FullName { get; set; }
+        [Display(Name= "Aktiv")]
+        public bool Active { get; set; }
+
+        [Display(Name="Gruppid")]
+        public int Group { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
